942dd51fa2091d41477afe5b64e590a49313c0aa
doniyorbek7376 <doniyorbek6439@gmail.com>
2021-02-06 19:17:27 +0500

    using repositories inside the controller

--- src/main/java/uz/doniyorbek7376/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/uz/doniyorbek7376/recipeapp/repositories/CategoryRepository.java
@@ -1,9 +1,11 @@
 package uz.doniyorbek7376.recipeapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import uz.doniyorbek7376.recipeapp.domain.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/uz/doniyorbek7376/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/uz/doniyorbek7376/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -1,9 +1,11 @@
 package uz.doniyorbek7376.recipeapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import uz.doniyorbek7376.recipeapp.domain.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
-
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
