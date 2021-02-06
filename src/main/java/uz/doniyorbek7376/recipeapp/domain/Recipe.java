6dcc3730cc8885805b7c873a71d9931c27ba21a2
doniyorbek7376 <doniyorbek6439@gmail.com>
2021-02-06 18:42:58 +0500

    add many-to-many relationship between recipe and category

--- src/main/java/uz/doniyorbek7376/recipeapp/domain/Recipe.java
+++ src/main/java/uz/doniyorbek7376/recipeapp/domain/Recipe.java
@@ -32,8 +32,20 @@ public class Recipe {
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public String getDescription() {
         return description;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 
     public Difficulty getDifficulty() {
