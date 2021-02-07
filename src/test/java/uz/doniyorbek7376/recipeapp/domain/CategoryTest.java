8e6117b2e2ec2050778172bb568a3fced25f880b
doniyorbek7376 <doniyorbek6439@gmail.com>
2021-02-07 17:33:04 +0500

    testing with juint

---
+++ src/test/java/uz/doniyorbek7376/recipeapp/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package uz.doniyorbek7376.recipeapp.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+    Category category;
+
+    @BeforeEach
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    public void getId() throws Exception {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+        String description = "Some random description";
+        category.setDescription(description);
+        assertEquals(description, category.getDescription(), "Get description");
+
+    }
+
+}
---
+++ src/test/java/uz/doniyorbek7376/recipeapp/services/RecipeServiceImpTest.java
@@ -0,0 +1,44 @@
+package uz.doniyorbek7376.recipeapp.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import uz.doniyorbek7376.recipeapp.domain.Recipe;
+import uz.doniyorbek7376.recipeapp.repositories.RecipeRepository;
+
+public class RecipeServiceImpTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
